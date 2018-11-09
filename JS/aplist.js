ac0946562049221770eced727ed703c83a539e59
wang926454 <1107224733@qq.com>
2018-11-09 10:54:54 +0800

    修改音乐播放器界面

---
+++ JS/aplist.js
@@ -0,0 +1,492 @@
+// 我的喜欢
+var love = [{
+    name: 'Towers',
+    artist: 'Frida',
+    url: 'https://res.wang64.cn/Music/towers.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },{
+    name: 'Waiting For Love',
+    artist: 'Avicii',
+    url: 'https://res.wang64.cn/Music/waitingforlove.mp3',
+    cover: 'https://res.wang64.cn/Music/image/waitingforlove.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/waitingforlove.lrc'
+  },
+  {
+    name: 'Nevada',
+    artist: 'Vicetone/Cozi Zuehlsdorff',
+    url: 'https://res.wang64.cn/Music/nevada.mp3',
+    cover: 'https://res.wang64.cn/Music/image/nevada.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/nevada.lrc'
+  },
+  {
+    name: 'Aeon',
+    artist: 'JJD',
+    url: 'https://res.wang64.cn/Music/aeon.mp3',
+    cover: 'https://res.wang64.cn/Music/image/aeon.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/aeon.lrc'
+  },
+  {
+    name: 'Trip',
+    artist: 'Axero',
+    url: 'https://res.wang64.cn/Music/trip.mp3',
+    cover: 'https://res.wang64.cn/Music/image/trip.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/trip.lrc'
+  },
+  {
+    name: 'Horizon',
+    artist: 'Janji',
+    url: 'https://res.wang64.cn/Music/horizon.mp3',
+    cover: 'https://res.wang64.cn/Music/image/horizon.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/horizon.lrc'
+  },
+  {
+    name: '五月雨变奏电音',
+    artist: '者思范Remix',
+    url: 'https://res.wang64.cn/Music/wuyueyu.mp3',
+    cover: 'https://res.wang64.cn/Music/image/wuyueyu.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/wuyueyu.lrc'
+  },
+  {
+    name: 'Sunburst(亚索台词版)',
+    artist: 'Goi7',
+    url: 'https://res.wang64.cn/Music/sunburst.mp3',
+    cover: 'https://res.wang64.cn/Music/image/sunburst.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/sunburst.lrc'
+  },
+  {
+    name: 'Legends Never Die(Remix)',
+    artist: 'Alan Walker',
+    url: 'https://res.wang64.cn/Music/legendsneverdie.mp3',
+    cover: 'https://res.wang64.cn/Music/image/legendsneverdie.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/legendsneverdie.lrc'
+  },
+  {
+    name: 'RISE 登峰造极境',
+    artist: '英雄联盟',
+    url: 'https://res.wang64.cn/Music/rise.mp3',
+    cover: 'https://res.wang64.cn/Music/image/rise.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/rise.lrc'
+  },
+  {
+    name: 'Daisy',
+    artist: 'STEREO DIVE FOUNDATION',
+    url: 'https://res.wang64.cn/Music/daisy.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'INNOCENCE',
+    artist: '藍井エイル',
+    url: 'https://res.wang64.cn/Music/INNOCENCE.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'unravel',
+    artist: '凛として時雨',
+    url: 'https://res.wang64.cn/Music/unravel.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'Sincerely',
+    artist: '唐沢美帆',
+    url: 'https://res.wang64.cn/Music/sincerely.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '飛竜の騎士',
+    artist: '唐沢美帆',
+    url: 'https://res.wang64.cn/Music/feixiangdeqishi.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '空想フォレスト',
+    artist: '伊東歌詞太郎',
+    url: 'https://res.wang64.cn/Music/kongxiang.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '清明樱花祭',
+    artist: 'RSP',
+    url: 'https://res.wang64.cn/Music/RSP.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'なんでもないや',
+    artist: 'RADWIMPS',
+    url: 'https://res.wang64.cn/Music/RADWIMPS.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '1年2ヶ月20日',
+    artist: 'BRIGHT',
+    url: 'https://res.wang64.cn/Music/BRIGHT.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '光るなら',
+    artist: 'Goose house',
+    url: 'https://res.wang64.cn/Music/goosehouseguang.mp3',
+    cover: 'https://res.wang64.cn/Music/image/goosehouseguang.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/goosehouseguang.lrc'
+  },
+  {
+    name: '風に薫る夏の記憶',
+    artist: 'AAA',
+    url: 'https://res.wang64.cn/Music/aaa.mp3',
+    cover: 'https://res.wang64.cn/Music/image/aaa.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/aaa.lrc'
+  },
+  {
+    name: '永久指针',
+    artist: '海贼王',
+    url: 'https://res.wang64.cn/Music/yongjiuzhizhen.mp3',
+    cover: 'https://res.wang64.cn/Music/image/yongjiuzhizhen.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/yongjiuzhizhen.lrc'
+  },
+  {
+    name: '一直很安静',
+    artist: '阿桑',
+    url: 'https://res.wang64.cn/Music/yizhihenanjing.mp3',
+    cover: 'https://res.wang64.cn/Music/image/yizhihenanjing.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/yizhihenanjing.lrc'
+  },
+  {
+    name: '空空如也',
+    artist: '胡66',
+    url: 'https://res.wang64.cn/Music/kongkongruye.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '侧脸',
+    artist: '于果',
+    url: 'https://res.wang64.cn/Music/celian.mp3',
+    cover: 'https://res.wang64.cn/Music/image/celian.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/celian.lrc'
+  },
+  {
+    name: '暧昧',
+    artist: '薛之谦',
+    url: 'https://res.wang64.cn/Music/aimei.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '一半',
+    artist: '薛之谦',
+    url: 'https://res.wang64.cn/Music/yiban.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '我好像在哪见过你',
+    artist: '薛之谦',
+    url: 'https://res.wang64.cn/Music/wohaoxiangzaina.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '为你我受冷风吹',
+    artist: '大壮',
+    url: 'https://res.wang64.cn/Music/weiniwoshou.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '你要的全拿走',
+    artist: '胡彦斌',
+    url: 'https://res.wang64.cn/Music/niyaodequan.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '我们不该这样的',
+    artist: '张赫宣',
+    url: 'https://res.wang64.cn/Music/womenbugai.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '走着走着就散了',
+    artist: '庄心妍',
+    url: 'https://res.wang64.cn/Music/zouzhezouzhe.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '从别后(Tvsize)',
+    artist: '流浪的蛙蛙',
+    url: 'https://res.wang64.cn/Music/congbiehou.mp3',
+    cover: 'https://res.wang64.cn/Music/image/congbiehou.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/congbiehou.lrc'
+  }];
+
+
+// 全部列表
+var list = [{
+    name: 'Towers',
+    artist: 'Frida',
+    url: 'https://res.wang64.cn/Music/towers.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },{
+    name: 'Waiting For Love',
+    artist: 'Avicii',
+    url: 'https://res.wang64.cn/Music/waitingforlove.mp3',
+    cover: 'https://res.wang64.cn/Music/image/waitingforlove.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/waitingforlove.lrc'
+  },
+  {
+    name: 'Nevada',
+    artist: 'Vicetone/Cozi Zuehlsdorff',
+    url: 'https://res.wang64.cn/Music/nevada.mp3',
+    cover: 'https://res.wang64.cn/Music/image/nevada.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/nevada.lrc'
+  },
+  {
+    name: 'Aeon',
+    artist: 'JJD',
+    url: 'https://res.wang64.cn/Music/aeon.mp3',
+    cover: 'https://res.wang64.cn/Music/image/aeon.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/aeon.lrc'
+  },
+  {
+    name: 'Trip',
+    artist: 'Axero',
+    url: 'https://res.wang64.cn/Music/trip.mp3',
+    cover: 'https://res.wang64.cn/Music/image/trip.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/trip.lrc'
+  },
+  {
+    name: 'Horizon',
+    artist: 'Janji',
+    url: 'https://res.wang64.cn/Music/horizon.mp3',
+    cover: 'https://res.wang64.cn/Music/image/horizon.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/horizon.lrc'
+  },
+  {
+    name: '五月雨变奏电音',
+    artist: '者思范Remix',
+    url: 'https://res.wang64.cn/Music/wuyueyu.mp3',
+    cover: 'https://res.wang64.cn/Music/image/wuyueyu.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/wuyueyu.lrc'
+  },
+  {
+    name: 'Sunburst(亚索台词版)',
+    artist: 'Goi7',
+    url: 'https://res.wang64.cn/Music/sunburst.mp3',
+    cover: 'https://res.wang64.cn/Music/image/sunburst.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/sunburst.lrc'
+  },
+  {
+    name: 'Legends Never Die(Remix)',
+    artist: 'Alan Walker',
+    url: 'https://res.wang64.cn/Music/legendsneverdie.mp3',
+    cover: 'https://res.wang64.cn/Music/image/legendsneverdie.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/legendsneverdie.lrc'
+  },
+  {
+    name: 'RISE 登峰造极境',
+    artist: '英雄联盟',
+    url: 'https://res.wang64.cn/Music/rise.mp3',
+    cover: 'https://res.wang64.cn/Music/image/rise.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/rise.lrc'
+  },
+  {
+    name: 'STYX HELIX',
+    artist: 'MYTH & ROID',
+    url: 'https://res.wang64.cn/Music/STYXHELIX.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'HYDRA',
+    artist: 'MYTH & ROID',
+    url: 'https://res.wang64.cn/Music/HYDRA.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'Daisy',
+    artist: 'STEREO DIVE FOUNDATION',
+    url: 'https://res.wang64.cn/Music/daisy.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'Perfect Time',
+    artist: '小林未郁',
+    url: 'https://res.wang64.cn/Music/perfecttime.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'INNOCENCE',
+    artist: '藍井エイル',
+    url: 'https://res.wang64.cn/Music/INNOCENCE.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'unravel',
+    artist: '凛として時雨',
+    url: 'https://res.wang64.cn/Music/unravel.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'unravel - 不插电版',
+    artist: '凛として時雨',
+    url: 'https://res.wang64.cn/Music/unravelno.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'Sincerely',
+    artist: '唐沢美帆',
+    url: 'https://res.wang64.cn/Music/sincerely.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '飛竜の騎士',
+    artist: '唐沢美帆',
+    url: 'https://res.wang64.cn/Music/feixiangdeqishi.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '空想フォレスト',
+    artist: '伊東歌詞太郎',
+    url: 'https://res.wang64.cn/Music/kongxiang.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '清明樱花祭',
+    artist: 'RSP',
+    url: 'https://res.wang64.cn/Music/RSP.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: 'なんでもないや',
+    artist: 'RADWIMPS',
+    url: 'https://res.wang64.cn/Music/RADWIMPS.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '1年2ヶ月20日',
+    artist: 'BRIGHT',
+    url: 'https://res.wang64.cn/Music/BRIGHT.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '光るなら',
+    artist: 'Goose house',
+    url: 'https://res.wang64.cn/Music/goosehouseguang.mp3',
+    cover: 'https://res.wang64.cn/Music/image/goosehouseguang.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/goosehouseguang.lrc'
+  },
+  {
+    name: '風に薫る夏の記憶',
+    artist: 'AAA',
+    url: 'https://res.wang64.cn/Music/aaa.mp3',
+    cover: 'https://res.wang64.cn/Music/image/aaa.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/aaa.lrc'
+  },
+  {
+    name: '永久指针',
+    artist: '海贼王',
+    url: 'https://res.wang64.cn/Music/yongjiuzhizhen.mp3',
+    cover: 'https://res.wang64.cn/Music/image/yongjiuzhizhen.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/yongjiuzhizhen.lrc'
+  },
+  {
+    name: '一直很安静',
+    artist: '阿桑',
+    url: 'https://res.wang64.cn/Music/yizhihenanjing.mp3',
+    cover: 'https://res.wang64.cn/Music/image/yizhihenanjing.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/yizhihenanjing.lrc'
+  },
+  {
+    name: '空空如也',
+    artist: '胡66',
+    url: 'https://res.wang64.cn/Music/kongkongruye.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '侧脸',
+    artist: '于果',
+    url: 'https://res.wang64.cn/Music/celian.mp3',
+    cover: 'https://res.wang64.cn/Music/image/celian.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/celian.lrc'
+  },
+  {
+    name: '暧昧',
+    artist: '薛之谦',
+    url: 'https://res.wang64.cn/Music/aimei.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '一半',
+    artist: '薛之谦',
+    url: 'https://res.wang64.cn/Music/yiban.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '我好像在哪见过你',
+    artist: '薛之谦',
+    url: 'https://res.wang64.cn/Music/wohaoxiangzaina.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '为你我受冷风吹',
+    artist: '大壮',
+    url: 'https://res.wang64.cn/Music/weiniwoshou.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '你要的全拿走',
+    artist: '胡彦斌',
+    url: 'https://res.wang64.cn/Music/niyaodequan.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '我们不该这样的',
+    artist: '张赫宣',
+    url: 'https://res.wang64.cn/Music/womenbugai.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '走着走着就散了',
+    artist: '庄心妍',
+    url: 'https://res.wang64.cn/Music/zouzhezouzhe.mp3',
+    cover: 'https://res.wang64.cn/Music/image/xxx.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/xxx.lrc'
+  },
+  {
+    name: '从别后(Tvsize)',
+    artist: '流浪的蛙蛙',
+    url: 'https://res.wang64.cn/Music/congbiehou.mp3',
+    cover: 'https://res.wang64.cn/Music/image/congbiehou.png',
+    lrc: 'https://res.wang64.cn/Music/lrc/congbiehou.lrc'
+  }];
